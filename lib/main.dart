5ff3a5721e9ef51f796f2e05fecf9684507d4197
pelinvarol <pelinsenavarol@eskisehir.edu.tr>
2023-01-18 00:59:10 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/root_page.dart';
 import 'package:plant_app/screens/launch_screen.dart';
-import 'package:provider/provider.dart';
-import 'models/cart_model.dart';
-import 'models/favorites_model.dart';
-import 'models/tab_model.dart';
 
 void main() {
   runApp(const MyApp());
